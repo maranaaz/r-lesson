92860f54499c848c8e68c28d76f2a6acad282e6a
renaldoc <liwen7@gmail.com>
2016-11-06 14:21:14 -0700

    new version

--- my_second_r.r
+++ my_second_r.r
@@ -9,6 +9,25 @@ age
 
 x <- c(1,2,3)
 y <- c(3,2,1)
-plot
+plotgapminder[1,1]
+
 help(plot)
 ?c
+cd <path/to/workdir>
+  
+setwd("~/Desktop/r-novice-inflammation/")
+read.csv("data/inflammation-01.cvs", header = FALSE)
+age,-2
+ageabc,-10
+abc
+abc,-10
+abc<-10
+abc
+bird <-100
+bird
+
+
+as.is=1
+ifelse()
+help("ifelse")
+dat[]
